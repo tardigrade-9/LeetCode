ea04111247e6442f84cb91535914d5b0df5f7e07
Akshay Gowda <55308877+tardigrade-9@users.noreply.github.com>
2023-09-25 16:31:09 -0600

    Time: 1 ms, Memory: 44.6 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,12 +1,10 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i+1<nums.length;i=i+2){
-            if(nums[i]==nums[i+1]){
-                continue;
-            }
-            return nums[i];
+        public int singleNumber(int[] nums) {
+        int a = 0;
+        //bit manipulation
+        for(int num:nums){
+            a = a^num;
         }
-        return nums[nums.length-1];
+        return a;
     }
 }
