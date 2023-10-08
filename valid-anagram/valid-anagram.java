0707a3b30b179ca063f4cb2c98d4dfdbc7699e1c
Akshay Gowda <55308877+tardigrade-9@users.noreply.github.com>
2023-10-07 23:12:23 -0600

    Time: 2 ms, Memory: 43.3 MB - LeetHub

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -4,14 +4,14 @@ class Solution {
             return false;
         char[] sc = s.toCharArray();
         char[] tc = t.toCharArray();
-        int[] sCount = new int[26], tCount = new int[26];
+        int[] count = new int[26];
         for(int i=0;i<sc.length;i++){
-            sCount[sc[i]-'a']++;
-            tCount[tc[i]-'a']++;
+            count[sc[i]-'a']++;
+            count[tc[i]-'a']--;
         }
         
         for(int i=0;i<26;i++){
-            if(sCount[i]!=tCount[i])
+            if(count[i]!=0)
                 return false;
         }
         return true;
