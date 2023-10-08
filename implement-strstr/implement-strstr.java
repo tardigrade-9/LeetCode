48c79eed4522b34f3e191a48b8bb44bb4fe4d5a6
Akshay Gowda <55308877+tardigrade-9@users.noreply.github.com>
2023-10-08 13:22:49 -0600

    Time: 0 ms, Memory: 40.5 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -1,5 +1,21 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        if(needle.length() > haystack.length())
+            return -1;
+        char[] ch = haystack.toCharArray();
+        char[] cn = needle.toCharArray();
+        int n = ch.length;
+        int m = cn.length;
+        int i =0,j=0;
+        while(i+j<n && j<m){
+            if(ch[i+j]==cn[j]){
+                j++;
+            }else{
+                i=i+1;
+                j=0;
+            }
+        }
+        
+        return j==m?i:-1;
     }
 }
